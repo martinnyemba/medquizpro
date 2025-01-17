9c48ec7cf4f1c9ce0aad36eb164cc351d43af833
martinnyemba <martinnyemba@gmail.com>
2025-01-17 08:17:36 +0200

    Update tests/__init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""Test package initialization"""
+import os
+import sys
+
+# Add the parent directory to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
